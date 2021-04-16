7eb00d1f1831c3f32be95379dae6ee5ee5a131b2
Robert Minchin <robert@alminit.com>
2021-04-16 10:03:51 +0100

    Add SQL Bus Matrix

---
+++ Documentation/Report/Stored Procedures/upGetSqlSchema.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [report].[upGetSqlSchema] (
+	@Server VARCHAR(255)
+	,@DatabaseName VARCHAR(255)
+	)
+AS
+BEGIN
+	SELECT DISTINCT SchemaName
+	FROM (
+		SELECT SERVERNAME
+			,DatabaseName
+			,schemaName
+		FROM dbo.vwChildObjects
+		
+		UNION ALL
+		
+		SELECT SERVERNAME
+			,DatabaseName
+			,ParentschemaName
+		FROM dbo.vwChildObjects
+		) AS T
+	WHERE SchemaName IS NOT NULL
+		AND DatabaseName = @DatabaseName
+		AND SERVERNAME = @Server;
+END;
