57e0780f032709591cc90558de47739576cba7d6
Robert Minchin <robert@alminit.com>
2021-04-29 15:03:20 +0100

    Show Load Date of Database on Reports

--- Documentation/report/Stored Procedures/upGetSQLDatabases.sql
+++ Documentation/report/Stored Procedures/upGetSQLDatabases.sql
@@ -4,4 +4,5 @@ AS
 SELECT DISTINCT ServerName
 	,DatabaseName
 	,CAST(d.DocumentationDescription AS VARCHAR(MAX))  as Description 
+	,d.StagingDateTime as DocumentationLoadDate
 FROM Staging.DatabaseInformation d
